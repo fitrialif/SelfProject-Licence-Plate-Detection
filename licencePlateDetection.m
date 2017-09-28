65a57b7141dc2fafcd38462ea6c4bfbcc9793acc
txlim96 <30485660+txlim96@users.noreply.github.com>
2017-09-28 15:30:32 +0800

    Add files via upload

--- licencePlateDetection.m
+++ licencePlateDetection.m
@@ -1,5 +1,13 @@
 clear;
 clc;
+
+refImagePath = 'C:\Users\Nelson\Desktop\Machine Vision Practical\Machine Vision Lab 2\video frames\frame352.jpg';
+referenceImage = imread(refImagePath);
+BWRef = rgb2gray(referenceImage);
+
+threshold = 0.18;
+%%
+% save frames
 % v = VideoReader('video.asf');
 
 % for i = 1: v.NumberofFrames
@@ -7,21 +15,59 @@ clc;
 %     img = read(v, i);
 %     imwrite(img, filename);
 % end
-% take an image as reference
-% subtract input iamge with reference image to locate car location
 
-refImagePath = 'C:\Users\Nelson\Desktop\Machine Vision Practical\Machine Vision Lab 2\video frames\frame352.jpg';
-referenceImage = imread(refImagePath);
-BWRef = rgb2gray(referenceImage);
+%%
+% edit frame rate
+% v = VideoReader('video.asf');
+% writer = VideoWriter('video(1)');
+% writer.FrameRate = 5;     %change frame rate
+% open(writer);
+% 
+% for i = 1: v.NumberofFrames
+%     img = read(v, i);
+%     writeVideo(writer, img);
+% end
+% 
+% close(writer);
 
-inputImagePath = 'C:\Users\Nelson\Desktop\Machine Vision Practical\Machine Vision Lab 2\video frames\frame600.jpg';
-inputImage = imread(inputImagePath);
-BWInput = rgb2gray(inputImage);
+%%
+% play edited video
+v = VideoReader('video(1).avi');
+for i = 1: v.NumberofFrames
+    inputImage = read(v, i);
+    BWInput = rgb2gray(inputImage);
+    imshow(BWInput); hold on;
+    
+    location = determine_car_location(BWRef, BWInput);
+    
+    BWcrop = imcrop(BWInput,...
+        [location(1, 1) location(2, 1) location(1, 3) location(2, 3)]);
+    
+    BWcrop = imbinarize(BWcrop, threshold);
+    updatedStruct = determine_car_plate(BWcrop, location);
+    for j = 1: size(updatedStruct, 2)
+        corners = detect_corners(BWInput, updatedStruct(j), location);
+    end
+end
 
-location = determine_car_location(BWRef, BWInput);
-
-figure; imshow(inputImage); hold on;
-
-rectangle('Position',...
-    [location(1, 1) location(2, 1) location(1, 3) location(2, 3)],...
-    'EdgeColor', 'r', 'LineWidth', 1.5);
+%%
+% myvi-70
+% waja-651
+% imageNum = 651;     %1-781
+% inputImagePath = 'C:\Users\Nelson\Desktop\Machine Vision Practical\Machine Vision Lab 2\video frames\frame';
+% inputImage = imread(strcat(inputImagePath, num2str(imageNum), '.jpg'));
+% BWInput = rgb2gray(inputImage);
+% 
+% figure; imshow(inputImage); hold on;
+% 
+% location = determine_car_location(BWRef, BWInput);
+% 
+% BWcrop = imcrop(BWInput,...
+%     [location(1, 1) location(2, 1) location(1, 3) location(2, 3)]);
+% % figure; imshow(BWcrop); hold on;
+% 
+% BWcrop = imbinarize(BWcrop, 0.2);
+% updatedStruct = determine_car_plate(BWcrop, location);
+% for i = 1: size(updatedStruct, 2)
+%     corners = detect_corners(BWInput, updatedStruct(i), location);
+% end
